27d6614db79ff2b13835024f9e0ac4a486d22f21
Your Name <email@example.com>
2017-04-25 22:44:20 +0800

    第一次上传

---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+
+Page({
+    data: {
+        imgUrls:[
+            'https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2382076389.jpg',
+            'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2382529006.jpg'
+        ]
+    }
+})
